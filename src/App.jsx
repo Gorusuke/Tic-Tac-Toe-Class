4fbebc5d7a4818ffd587092d2c0b55b1025d5673
Jose Andres <Gorusuke123@gmail.com>
2023-10-17 19:31:51 -0500

    Add missing files

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,73 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useState } from 'react';
+import confetti from 'canvas-confetti'
 import './App.css'
+import { Square } from './components/Square'
+import { TURNS } from './constants';
+import { checkWinner } from './logic/checkWinner'; 
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [board, setBoard] = useState(Array(9).fill(null));
+  const [turn, setTurn] = useState(TURNS.O);
+  const [winner, setWinner] = useState(null)
+
+  const updateBoard = (index) => {
+    if(board[index] !== null || winner) return
+    const newBoard = [...board]
+    newBoard[index] = turn
+    setBoard(newBoard)
+    const newTurn = turn === TURNS.O ? TURNS.X : TURNS.O
+    setTurn(newTurn)
+    const newWinner = checkWinner(newBoard)
+    if (newWinner) {
+      confetti()
+      setWinner(newWinner)
+    } else if (!newBoard.includes(null)) {
+      setWinner(false)
+    }
+  }
+
+  const resetGame = () => {
+    setBoard(Array(9).fill(null));
+    setTurn(TURNS.O);
+    setWinner(null)
+  }
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <main className='board'>
+      <h1>Tic Tac Toe</h1>
+      <button onClick={resetGame}>Empezar de nuevo</button>
+      <section className='game'>
+        {board.map((_, index) => 
+          <Square
+            key={index}
+            index={index}
+            updateBoard={updateBoard}
+          >
+            {board[index]}
+          </Square>
+        )}
+        <section className='turn'>
+          <Square isSelected={turn === TURNS.O}>{TURNS.O}</Square>
+          <Square isSelected={turn === TURNS.X}>{TURNS.X}</Square>
+        </section>
+      </section>
+      {
+        winner !== null &&
+        <section className='winner'>
+            <div className='text'>
+              <h2>{winner === false ? 'Empato' : 'Gano'}</h2>
+                {winner &&
+                  <header className='win'>
+                   <Square>{winner}</Square>
+                  </header>
+                }
+              <footer>
+                <button onClick={resetGame}>Empezar de nuevo</button>
+              </footer>
+            </div>    
+        </section>
+      }
+    </main>
   )
 }
 
